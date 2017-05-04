63659caf238f956c0c7a22733c65e33e2f4a445b
salarcode <salar2k@gmail.com>
2017-05-04 22:05:18 +0430

    Improving IsConnected.

--- TLSharp.Core/Network/TcpTransport.cs
+++ TLSharp.Core/Network/TcpTransport.cs
@@ -5,158 +5,167 @@ using System.Threading.Tasks;
 
 namespace TLSharp.Core.Network
 {
-    public delegate TcpClient TcpClientConnectionHandler(string address, int port);
+	public delegate TcpClient TcpClientConnectionHandler(string address, int port);
 
-    public class TcpTransport : IDisposable
-    {
-        private readonly TcpClient _tcpClient;
-        private int sendCounter = 0;
+	public class TcpTransport : IDisposable
+	{
+		private readonly TcpClient _tcpClient;
+		private int sendCounter = 0;
 
-        public TcpTransport(string address, int port, bool keepAlive = false, TcpClientConnectionHandler handler = null)
-        {
-            if (handler == null)
-            {
-                _tcpClient = new TcpClient();
+		public TcpTransport(string address, int port, bool keepAlive = false, TcpClientConnectionHandler handler = null)
+		{
+			if (handler == null)
+			{
+				_tcpClient = new TcpClient();
 
-                var ipAddress = IPAddress.Parse(address);
-                _tcpClient.Connect(ipAddress, port);
-            }
-            else
-                _tcpClient = handler(address, port);
+				var ipAddress = IPAddress.Parse(address);
+				_tcpClient.Connect(ipAddress, port);
+			}
+			else
+				_tcpClient = handler(address, port);
 
-            if (keepAlive)
-            {
-                // check every 500 millisecond
-                // allowed 1 second inactivity
-                SetKeepAlive(_tcpClient.Client, 1000, 500);
-            }
-        }
+			if (keepAlive)
+			{
+				// check every 500 millisecond
+				// allowed 1 second inactivity
+				SetKeepAlive(_tcpClient.Client, 1000, 500);
+			}
+		}
 
-        public async Task Send(byte[] packet)
-        {
-            if (!_tcpClient.Connected)
-                throw new InvalidOperationException("Client not connected to server.");
+		public async Task Send(byte[] packet)
+		{
+			if (!_tcpClient.Connected)
+				throw new InvalidOperationException("Client not connected to server.");
 
-            var tcpMessage = new TcpMessage(sendCounter, packet);
+			var tcpMessage = new TcpMessage(sendCounter, packet);
 
-            await _tcpClient.GetStream().WriteAsync(tcpMessage.Encode(), 0, tcpMessage.Encode().Length);
-            sendCounter++;
-        }
+			await _tcpClient.GetStream().WriteAsync(tcpMessage.Encode(), 0, tcpMessage.Encode().Length);
+			sendCounter++;
+		}
 
-        public async Task<TcpMessage> Receieve()
-        {
-            var stream = _tcpClient.GetStream();
+		public async Task<TcpMessage> Receieve()
+		{
+			var stream = _tcpClient.GetStream();
 
-            var packetLengthBytes = new byte[4];
-            if (await stream.ReadAsync(packetLengthBytes, 0, 4) != 4)
-                throw new InvalidOperationException("Couldn't read the packet length");
-            int packetLength = BitConverter.ToInt32(packetLengthBytes, 0);
+			var packetLengthBytes = new byte[4];
+			if (await stream.ReadAsync(packetLengthBytes, 0, 4) != 4)
+				throw new InvalidOperationException("Couldn't read the packet length");
+			int packetLength = BitConverter.ToInt32(packetLengthBytes, 0);
 
-            var seqBytes = new byte[4];
-            if (await stream.ReadAsync(seqBytes, 0, 4) != 4)
-                throw new InvalidOperationException("Couldn't read the sequence");
-            int seq = BitConverter.ToInt32(seqBytes, 0);
+			var seqBytes = new byte[4];
+			if (await stream.ReadAsync(seqBytes, 0, 4) != 4)
+				throw new InvalidOperationException("Couldn't read the sequence");
+			int seq = BitConverter.ToInt32(seqBytes, 0);
 
-            int readBytes = 0;
-            var body = new byte[packetLength - 12];
-            int neededToRead = packetLength - 12;
+			int readBytes = 0;
+			var body = new byte[packetLength - 12];
+			int neededToRead = packetLength - 12;
 
-            do
-            {
-                var bodyByte = new byte[packetLength - 12];
-                var availableBytes = await stream.ReadAsync(bodyByte, 0, neededToRead);
-                neededToRead -= availableBytes;
-                Buffer.BlockCopy(bodyByte, 0, body, readBytes, availableBytes);
-                readBytes += availableBytes;
-            }
-            while (readBytes != packetLength - 12);
+			do
+			{
+				var bodyByte = new byte[packetLength - 12];
+				var availableBytes = await stream.ReadAsync(bodyByte, 0, neededToRead);
+				neededToRead -= availableBytes;
+				Buffer.BlockCopy(bodyByte, 0, body, readBytes, availableBytes);
+				readBytes += availableBytes;
+			}
+			while (readBytes != packetLength - 12);
 
-            var crcBytes = new byte[4];
-            if (await stream.ReadAsync(crcBytes, 0, 4) != 4)
-                throw new InvalidOperationException("Couldn't read the crc");
-            int checksum = BitConverter.ToInt32(crcBytes, 0);
+			var crcBytes = new byte[4];
+			if (await stream.ReadAsync(crcBytes, 0, 4) != 4)
+				throw new InvalidOperationException("Couldn't read the crc");
+			int checksum = BitConverter.ToInt32(crcBytes, 0);
 
-            byte[] rv = new byte[packetLengthBytes.Length + seqBytes.Length + body.Length];
+			byte[] rv = new byte[packetLengthBytes.Length + seqBytes.Length + body.Length];
 
-            Buffer.BlockCopy(packetLengthBytes, 0, rv, 0, packetLengthBytes.Length);
-            Buffer.BlockCopy(seqBytes, 0, rv, packetLengthBytes.Length, seqBytes.Length);
-            Buffer.BlockCopy(body, 0, rv, packetLengthBytes.Length + seqBytes.Length, body.Length);
-            var crc32 = new Ionic.Crc.CRC32();
-            crc32.SlurpBlock(rv, 0, rv.Length);
-            var validChecksum = crc32.Crc32Result;
+			Buffer.BlockCopy(packetLengthBytes, 0, rv, 0, packetLengthBytes.Length);
+			Buffer.BlockCopy(seqBytes, 0, rv, packetLengthBytes.Length, seqBytes.Length);
+			Buffer.BlockCopy(body, 0, rv, packetLengthBytes.Length + seqBytes.Length, body.Length);
+			var crc32 = new Ionic.Crc.CRC32();
+			crc32.SlurpBlock(rv, 0, rv.Length);
+			var validChecksum = crc32.Crc32Result;
 
-            if (checksum != validChecksum)
-            {
-                throw new InvalidOperationException("invalid checksum! skip");
-            }
+			if (checksum != validChecksum)
+			{
+				throw new InvalidOperationException("invalid checksum! skip");
+			}
 
-            return new TcpMessage(seq, body);
-        }
+			return new TcpMessage(seq, body);
+		}
 
-        /// <summary>
-        /// Setting socket keep-alive
-        /// </summary>
-        /// <param name="sock"></param>
-        /// <param name="time">milliseconds of allowed inactivity</param>
-        /// <param name="interval">interval milliseconds on keep-alive checks</param>
-        /// <returns>was successfull?</returns>
-        private static bool SetKeepAlive(Socket sock, ulong time, ulong interval)
-        {
-            // "consts" to help understand calculations
-            const int bytesperlong = 4; // 32 / 8
-            const int bitsperbyte = 8;
+		/// <summary>
+		/// Setting socket keep-alive
+		/// </summary>
+		/// <param name="sock"></param>
+		/// <param name="time">milliseconds of allowed inactivity</param>
+		/// <param name="interval">interval milliseconds on keep-alive checks</param>
+		/// <returns>was successfull?</returns>
+		private static bool SetKeepAlive(Socket sock, ulong time, ulong interval)
+		{
+			// "consts" to help understand calculations
+			const int bytesperlong = 4; // 32 / 8
+			const int bitsperbyte = 8;
 
-            try
-            {
-                // resulting structure
-                byte[] SIO_KEEPALIVE_VALS = new byte[3 * bytesperlong];
+			try
+			{
+				// resulting structure
+				byte[] SIO_KEEPALIVE_VALS = new byte[3 * bytesperlong];
 
-                // array to hold input values
-                ulong[] input = new ulong[3];
+				// array to hold input values
+				ulong[] input = new ulong[3];
 
-                // put input arguments in input array
-                if (time == 0 || interval == 0) // enable disable keep-alive
-                    input[0] = (0UL); // off
-                else
-                    input[0] = (1UL); // on
+				// put input arguments in input array
+				if (time == 0 || interval == 0) // enable disable keep-alive
+					input[0] = (0UL); // off
+				else
+					input[0] = (1UL); // on
 
-                input[1] = (time); // time millis
-                input[2] = (interval); // interval millis
+				input[1] = (time); // time millis
+				input[2] = (interval); // interval millis
 
-                // pack input into byte struct
-                for (int i = 0; i < input.Length; i++)
-                {
-                    SIO_KEEPALIVE_VALS[i * bytesperlong + 3] = (byte)(input[i] >> ((bytesperlong - 1) * bitsperbyte) & 0xff);
-                    SIO_KEEPALIVE_VALS[i * bytesperlong + 2] = (byte)(input[i] >> ((bytesperlong - 2) * bitsperbyte) & 0xff);
-                    SIO_KEEPALIVE_VALS[i * bytesperlong + 1] = (byte)(input[i] >> ((bytesperlong - 3) * bitsperbyte) & 0xff);
-                    SIO_KEEPALIVE_VALS[i * bytesperlong + 0] = (byte)(input[i] >> ((bytesperlong - 4) * bitsperbyte) & 0xff);
-                }
-                // create bytestruct for result (bytes pending on server socket)
-                byte[] result = BitConverter.GetBytes(0);
+				// pack input into byte struct
+				for (int i = 0; i < input.Length; i++)
+				{
+					SIO_KEEPALIVE_VALS[i * bytesperlong + 3] = (byte)(input[i] >> ((bytesperlong - 1) * bitsperbyte) & 0xff);
+					SIO_KEEPALIVE_VALS[i * bytesperlong + 2] = (byte)(input[i] >> ((bytesperlong - 2) * bitsperbyte) & 0xff);
+					SIO_KEEPALIVE_VALS[i * bytesperlong + 1] = (byte)(input[i] >> ((bytesperlong - 3) * bitsperbyte) & 0xff);
+					SIO_KEEPALIVE_VALS[i * bytesperlong + 0] = (byte)(input[i] >> ((bytesperlong - 4) * bitsperbyte) & 0xff);
+				}
+				// create bytestruct for result (bytes pending on server socket)
+				byte[] result = BitConverter.GetBytes(0);
 
-                // write SIO_VALS to Socket IOControl
-                sock.IOControl(IOControlCode.KeepAliveValues, SIO_KEEPALIVE_VALS, result);
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-            return true;
-        }
+				// write SIO_VALS to Socket IOControl
+				sock.IOControl(IOControlCode.KeepAliveValues, SIO_KEEPALIVE_VALS, result);
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+			return true;
+		}
 
-        public bool IsConnected
-        {
-            get
-            {
-                return this._tcpClient.Connected;
-            }
-        }
+		private bool IsNetworkConnected()
+		{
+			try
+			{
+				return !(_tcpClient.Client.Poll(1, SelectMode.SelectRead) && _tcpClient.Client.Available == 0);
+			}
+			catch (SocketException) { return false; }
+		}
+
+		public bool IsConnected
+		{
+			get
+			{
+				return this._tcpClient.Connected && IsNetworkConnected();
+			}
+		}
 
 
-        public void Dispose()
-        {
-            if (_tcpClient.Connected)
-                _tcpClient.Close();
-        }
-    }
+		public void Dispose()
+		{
+			if (_tcpClient.Connected)
+				_tcpClient.Close();
+		}
+	}
 }
